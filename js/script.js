73191dd38c12c62e39422e0935cf2d2335ed3c5d
mahadihasan61 <atraitor61@gmail.com>
2023-03-01 10:29:45 +0600

    ok cool

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $(function(){
+//     $('.rev-tev').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: false,
+//         autoplaySpeed: 2000,
+//       });
+
+
+
+
+// })
